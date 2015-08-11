9734ebff9f91c3dd995e8211af5135e87ea69a4c
Max-Andre Zils <zilsmc@gmail.com>
2015-08-11 15:52:59 +0200

    added some gulp and sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,27 @@
+/*  gulpfile.js
+    Tasks:
+        - default :
+        - sass
+        - uglify
+        - concat
+        - optimize-images
+        - nodemon
+*/
+var
+    gulp    = require('gulp'),
+    gutil   = require('gulp-util'),
+    concat  = require('gulp-concat'),
+    uglify  = require('gulp-uglify'),
+    sass    = require('gulp-sass'),
+    nodemon = require('gulp-nodemon')
+;
 
+gulp.task('sass', function() {
+    gulp.src('./public/css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./public/css/*.scss', ['sass']);
+});
